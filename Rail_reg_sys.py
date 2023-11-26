338c751d6c64bdabf4e0ae519616cd7ba2ed22fd
Suvojoti Howlader <152045641+SuvoH05@users.noreply.github.com>
2023-11-26 20:37:34 +0530

    Update Rail_reg_sys.py

--- Rail_reg_sys.py
+++ Rail_reg_sys.py
@@ -1,23 +1,2 @@
-def vow(a):
-    p=len(a)
-    v=""
-    c=""
-    for i in range(p):
-        if (a[i]== "a" or a[i]== "e" or a[i]== "i" or a[i]== "o" or a[i]== "u") :
-            v+= a[i]
-        else:
-            c+=a[i]
-    vl = sorted(v)
-    s1=""
-    for i in vl:
-        s1+=i
-    fina = s1+c
-    final = fina+" "
-    return final
-def kiddo(s):
-    str1=s.split()
-    final1=""
-    for i in str1:
-        final1+=vow(i)
-    print(final1)
+#Railway Registration System 
 
