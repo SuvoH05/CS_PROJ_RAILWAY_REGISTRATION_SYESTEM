2e24b49bce57762f90f21ca273b5e78e3f4ced2a
Suvojoti Howlader <152045641+SuvoH05@users.noreply.github.com>
2023-12-02 01:05:36 +0530

    Added function for registration

--- Rail_reg_sys.py
+++ Rail_reg_sys.py
@@ -11,7 +11,7 @@ def login(rand,Sql):
     if captcha == rand:
         Sql.execute("select * from login")
         for i in Sql:
-            if i[0] == username and i[1] == password :
+            if i[0] == username and i[6] == password :
                 print ("Login Successful")
             else:
                 print ("Invalid Login Details")
@@ -27,9 +27,34 @@ def booking_ticket():
     Classes = input("Enter your preferred class: ")
     Catagory = input("Enter your catagory: ")
     
+def registration(myc):
+    Username = input("Enter Your Name: ")
+    age = int(input("Enter Your Age: "))
+    gender = input("Enter Your Gender: ")
+    address = input("Enter Your Address: ")
+    contact = int(input("Enter Your Contact Number: "))
+    email = input("Enter Your Email ID: ")
+    password = input("Enter a strong password: ")
+    confirm_pass = input("Confirm Password: ")
+
+    if password != confirm_pass:   
+        print("Passwords do not match.")
+        password = input("Re-enter new password: ")
+        confirm_pass = input("Re-confirm password: ")
+    elif len(password)<8 or len(password)>30:
+        print("Password should be between 8 and 30 characters long.")
+        password = input("Re-enter new password: ")
+        confirm_pass = input("Re-confirm password: ")
+        
+    info = "INSERT INTO registration VALUES('{}',{},'{}','{}',{},'{}','{}')".format(Username,age,gender,address,contact,email,password)
+    myc.execute(info)
+    myc.execute("commit")
+        
+
+
 mydb=mysql.connector.connect(host="localhost",user="root",password="2005",database="cs_proj")
 rand = "".join(random.choices(string.ascii_letters + string.digits, k=5))
 myc=mydb.cursor()
-
-#login(rand,myc)
-booking_ticket()
+login(rand,myc)
+#booking_ticket()
+#registration(myc)
